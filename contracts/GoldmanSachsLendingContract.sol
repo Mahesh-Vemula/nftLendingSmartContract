99f22b282f00f8e135aa6350eacc345b7f0e17f4
Mahesh Vemula <maheshvemula40@gmail.com>
2021-10-11 12:15:48 -0400

    Lending contract updates

--- contracts/GoldmanSachsLendingContract.sol
+++ contracts/GoldmanSachsLendingContract.sol
@@ -17,8 +17,8 @@ contract GoldmanSachsLendingContract {
 
 	LoanRequestInfo public loanRequestInfo;
 	LoanStatus loanApplicationStatus;
-	address lender;
-	address borrower;
+	address payable public lender;
+	address payable public borrower;
 
 	constructor (address nftTokenAddress,
 		uint256 tokenId,
@@ -32,7 +32,7 @@ contract GoldmanSachsLendingContract {
 	function lenderApproveLoanRequest() public payable returns (string memory){
 		uint256 nftValue = GoldmanSachsNFT(loanRequestInfo.nftTokenAddress).getNFTValue(loanRequestInfo.tokenId);
 		require(msg.value >= loanRequestInfo.loanAmount);
-		lender = msg.sender;
+		lender = payable(msg.sender);
 		if(nftValue > (loanRequestInfo.loanAmount * 7/10)){
 			loanApplicationStatus = LoanStatus.APPROVED;
 			return "Loan Approved";
@@ -40,6 +40,31 @@ contract GoldmanSachsLendingContract {
 			loanApplicationStatus = LoanStatus.DENIED;
 			return "Loan Request exceeded required collateral value";
 		}
+	}
+
+	function borrowerTakeDisbursement() public{
+		borrower = payable( GoldmanSachsNFT(loanRequestInfo.nftTokenAddress).ownerOf(loanRequestInfo.tokenId));
+		borrower.transfer(loanRequestInfo.loanAmount);
+		loanApplicationStatus = LoanStatus.DISBURSED;
+	} 
+
+	function payLoanDue() public {
+
+	}
+
+	function initiateLiquidation() public {
+
+	}
+
+	function getBalanceOfContract() public view returns (uint256){
+		return address(this).balance;
+	}
+
+	function getLoanStatus() public view returns (string memory){
+		if(loanApplicationStatus == LoanStatus.APPROVED){
+			return "Loan request approved and ready to disburse";
+		}
+		return "Request Initiated";
 	}
 
 }
--- test/GoldmanSachsNFTTest.js
+++ test/GoldmanSachsNFTTest.js
@@ -1,6 +1,9 @@
 const GoldmanSachsNFT = artifacts.require('GoldmanSachsNFT.sol');
 
-contract ('GoldmanSachsNFT', () => {
+contract ('GoldmanSachsNFT', (accounts) => {
+
+	const toBN = web3.utils.toBN;
+
 	it('New NFT created', async() => {
 		const gsnft = await GoldmanSachsNFT.new();
 		await gsnft.createNFT(100);
@@ -18,5 +21,17 @@ contract ('GoldmanSachsNFT', () => {
 		await gsnft.createNFT(100);
 		const nftsCount = await gsnft.symbol();
 		assert(nftsCount == 'GSNFT');
+	});
+	it('Add new approval', async() => {
+		const gsnft = await GoldmanSachsNFT.new();
+		tokenId = await gsnft.createNFT(100);
+		console.log(accounts);
+		//address addressForApprove = "0x78731D3Ca6b7E34aC0F824c42a7cC18A495cabaB"
+		//var addressForApprove = accounts[1];
+		gsnft.approve( accounts[1], tokenId);
+		//console.log(BigNumber(await gsnft.getApproved(tokenId)));
+		//await gsnft.getApproved(tokenId);
+		//assert(tokenApprovedAddress == accounts[1]);
+		//assert(true);
 	});
 });
