0115ad9004512d53df89447078858d189dd78aea
Mahesh Vemula <maheshvemula40@gmail.com>
2021-10-11 19:13:47 -0400

    Removing NFT approval on loan payment

--- contracts/GoldmanSachsLendingContract.sol
+++ contracts/GoldmanSachsLendingContract.sol
@@ -75,7 +75,7 @@ contract GoldmanSachsLendingContract {
 		loanRequestInfo.USDCTokenAddress.transferFrom(msg.sender, lender, loanBalance());
 		loanApplicationStatus = LoanStatus.PAYED;
 		GoldmanSachsNFT(loanRequestInfo.nftTokenAddress).updateCollateralStatus(loanRequestInfo.tokenId, false);
-		GoldmanSachsNFT(loanRequestInfo.nftTokenAddress).approve(borrower, loanRequestInfo.tokenId);
+		GoldmanSachsNFT(loanRequestInfo.nftTokenAddress).approve(address(0x0), loanRequestInfo.tokenId);
 	}
 
 	function initiateLiquidation() public payable{
