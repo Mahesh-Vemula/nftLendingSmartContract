6ecb9193011879e06313ac7ac82951b980da95d6
Mahesh Vemula <maheshvemula40@gmail.com>
2021-10-16 21:42:34 -0400

    Updated files

---
+++ contracts/NFT.sol
@@ -0,0 +1,43 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract NFT is ERC721 {
+
+	uint256 public countNFTs;
+	mapping(uint256 => uint256) private nftPrices;
+	mapping(uint256 => bool) private nftCollateralStatus;
+
+	constructor ()  ERC721 ("GoldmanSachsNFT", "GSNFT"){
+		countNFTs = 0;
+	}
+
+	function createNFT(uint256 initialPrice) public returns (uint256) {
+		uint256 newItemId = countNFTs;
+		_safeMint(msg.sender, newItemId);
+		nftPrices[newItemId] = initialPrice;
+		nftCollateralStatus[newItemId] = false;
+		countNFTs = countNFTs + 1;
+		return newItemId;
+	}
+
+
+	function getNFTValue(uint256 tokenId) public view returns (uint256) {
+		return nftPrices[tokenId];
+	}
+	
+	function isUnderCollateral(uint256 tokenId) public view returns (bool) {
+		return nftCollateralStatus[tokenId];
+	}
+	
+	function updateCollateralStatus(uint256 tokenId, bool status) public returns (string memory){
+		nftCollateralStatus[tokenId] = status;
+		if(nftCollateralStatus[tokenId] == status){
+			return "Success";
+		}else{
+			return "Failed to update status";
+		}
+	}
+
+}
---
+++ test/NFTTest.js
@@ -0,0 +1,30 @@
+const GoldmanSachsNFT = artifacts.require('GoldmanSachsNFT.sol');
+
+contract ('GoldmanSachsNFT', (accounts) => {
+
+	it('New NFT created', async() => {
+		const gsnft = await GoldmanSachsNFT.new();
+		await gsnft.createNFT(100);
+		const nftsCount = await gsnft.countNFTs();
+		assert(nftsCount > 0);
+	});
+	it('NFT has correct name', async() => {
+		const gsnft = await GoldmanSachsNFT.new();
+		await gsnft.createNFT(100);
+		const nftsCount = await gsnft.name();
+		assert(nftsCount == 'GoldmanSachsNFT');
+	});
+	it('NFT has correct symbol', async() => {
+		const gsnft = await GoldmanSachsNFT.new();
+		await gsnft.createNFT(100);
+		const nftsCount = await gsnft.symbol();
+		assert(nftsCount == 'GSNFT');
+	});
+	it('Add new approval', async() => {
+		const gsnft = await GoldmanSachsNFT.new();
+		await gsnft.createNFT(100);
+		await gsnft.approve(accounts[1], 0, {from: accounts[0]});
+		let approvedAddress = await gsnft.getApproved(0) ;
+		assert(approvedAddress == accounts[1]);
+	});
+});
